931fb0a0b906fb9015c06440c440d8684570ceee
Francesco Siddi <francesco.siddi@gmail.com>
2015-03-01 00:55:15 +0100

    Command to populate for testing

--- blender-today/manage.py
+++ blender-today/manage.py
@@ -1,6 +1,11 @@
-from application import app, db
 from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.migrate import Migrate
+from flask.ext.migrate import MigrateCommand
+from application import app
+from application import db
+from application.modules.posts.model import Post
+from application.modules.posts.model import PostRating
+from application.helpers import encode_id
 
 migrate = Migrate(app, db)
 manager = Manager(app)
@@ -16,5 +21,30 @@ def runserver():
     os.environ['DEBUG'] = 'true'
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
     app.run()
+
+@manager.command
+def populate_for_testing():
+    for x in range(50):
+        post = Post(
+            user_id=1,
+            category_id=1,
+            post_type_id=1,
+            title='test '+str(x),
+            slug='the-slug',
+            content='<p>content</p>')
+        db.session.add(post)
+        db.session.commit()
+        post.uuid = encode_id(post.id)
+        db.session.commit()
+        post_rating = PostRating(
+            post_id=post.id,
+            positive=0,
+            negative=0
+            )
+        db.session.add(post_rating)
+        post.update_hot()
+        db.session.commit()
+        print x
+
 
 manager.run()
