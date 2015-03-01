926d51907c475e3f144491590348c25f41b2cc8e
Pablo Vazquez <venomgfx@gmail.com>
2015-03-01 03:02:47 +0100

    Remove email from settings

--- blender-today/application/modules/settings/forms.py
+++ blender-today/application/modules/settings/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 
 class ProfileForm(Form):
-    email = StringField('Email', validators=[DataRequired()])
+    #email = StringField('Email', validators=[DataRequired()])
     first_name = StringField('First name')
     last_name = StringField('Last name')
     username = StringField('Username')
