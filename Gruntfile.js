de098d476902da421fdc7f28cf0b7413e7106443
Pablo Vazquez <venomgfx@gmail.com>
2015-03-03 00:04:18 +0100

    Uglify JS with Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,19 +13,29 @@ module.exports = function(grunt) {
             }
         },
 
+        uglify: {
+            all_src : {
+              src : 'blender-today/application/static/js/uglify/*.js',
+              dest : 'blender-today/application/static/js/theuniverse.min.js'
+            }
+        },
+
         autoprefixer: {
             no_dest: { src: 'blender-today/application/static/css/main.css' }
         },
 
         watch: {
             files: ['blender-today/application/static/sass/main.sass'],
+            js:  { files: 'blender-today/application/static/js/uglify/*.js', tasks: [ 'uglify' ] },
             tasks: ['sass', 'autoprefixer'],
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-autoprefixer');
 
     grunt.registerTask('default', ['sass', 'autoprefixer']);
+    grunt.registerTask('default', [ 'uglify' ]);
 };
---
+++ blender-today/application/static/js/uglify/today-submit.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+        var csrftoken = $('meta[name=csrf-token]').attr('content');
+
+        $.ajaxSetup({
+          beforeSend: function(xhr, settings) {
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+              xhr.setRequestHeader("X-CSRFToken", csrftoken)
+            }
+          }
+        });
+
+        $("#submit_post_form").on("submit", function (e) {
+            e.preventDefault();
+            $('.post-submit-field-submit').html('<i class="fa fa-spinner fa-spin"></i> <i>Submitting...</i>');
+            var data = $(this).serialize();
+            url = $(this).attr('action');
+            var formData = new FormData($(this)[0]);
+            $.ajax({
+                  url: url,
+                  type: 'POST',
+                  data: formData,
+                  processData: false,
+                  contentType: false,
+                  cache: false
+                })
+            .done(function(data){
+                console.log(data['post_url']);
+                $('.post-submit-field-submit').css('background-color', '#5cb85c');
+                $('.post-submit-field-submit').html('<i class="fa fa-check"></i> Success!');
+                window.location.replace(data['post_url']);
+            })
+            .fail(function(data){
+              console.log('We have a problemos.');
+              $('.post-submit-field-submit').html('<i class="fa fa-frown-o fa-spin"></i> Problemo');
+            });
+        });
+
+        (function() {
+          var dlgtrigger = document.querySelector( '[data-dialog]' ),
+            somedialog = document.getElementById( dlgtrigger.getAttribute( 'data-dialog' ) ),
+            dlg = new DialogFx( somedialog );
+          dlgtrigger.addEventListener( 'click', dlg.toggle.bind(dlg) );
+        })();
+
+        CKEDITOR.replace( 'post_content', {
+          removePlugins: 'elementspath',
+          resize_enabled: false
+        });
+        CKEDITOR.config.forcePasteAsPlainText = true;
+        CKEDITOR.instances['post_content'].on('change', function() { CKEDITOR.instances['post_content'].updateElement() });
+
+        $('.select-change').click(function(){
+          $('#post_type_id').val($(this).data('val'));
+          $('.select-change').each(function() {
+            $(this).removeClass('activato');
+          });
+
+          $(this).addClass('activato');
+
+          $('.post-submit-div-title').show();
+          $('.post-submit-div-category').show();
+          $('.post-submit-div-picture').show();
+          $('.post-submit-div-submit').show();
+
+          if ($('#post_type_id').val() == 1){
+            $('.post-submit-link').show();
+            $('.post-submit-article').hide();
+          };
+          if ($('#post_type_id').val() == 2){
+            $('.post-submit-link').hide();
+            $('.post-submit-article').show();
+          };
+        });
+});
