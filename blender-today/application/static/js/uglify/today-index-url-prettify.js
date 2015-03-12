c0de9e333188bc755f4daa999f95e8a4fe1e09b4
Pablo Vazquez <venomgfx@gmail.com>
2015-03-12 18:47:06 +0100

    Add URL to post view as well

--- blender-today/application/static/js/uglify/today-index-url-prettify.js
+++ blender-today/application/static/js/uglify/today-index-url-prettify.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
- 	$('.post-index-item-title-url').each(function(){
+ 	$('.post-title-url').each(function(){
 		var link = $(this).text();
 		var pretty = link.replace(/.*?:\/\//g, "").replace(/^www./, "").replace(/\/$/, "");
 		var prettier = pretty.substr(0, pretty.indexOf('/'))
