2d9ecfecf8754afc7fb32d178da754e136c0682e
Pablo Vazquez <venomgfx@gmail.com>
2015-03-15 20:32:28 +0100

    Move javascript for social toggle to it's own file

---
+++ blender-today/application/static/js/uglify/today-social.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#social-toggle').click(function(){
+	    var pops = $('a');
+
+	    if ($(this).hasClass('activato')){
+		    $(this).removeClass('activato');
+		    $(this).html('<i class="fa fa-share-alt"></i>');
+
+		    $('#social').find(pops).each(function(i){
+			    $(this).animate({'right' : (5 * i) * -1, 'opacity' : 0});
+		    });
+
+	    } else {
+		    $(this).addClass('activato');
+
+		    $('#social').find(pops).each(function(i){
+			    $(this).animate({'right' : 40 * i, 'opacity' : 1.0});
+		    });
+
+		    $(this).html('<i class="fa fa-times"></i>');
+	    };
+    }); // social-toggle
+});
