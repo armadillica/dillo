189b563af5cc4e7a846a37d3b441f1982f51b137
Pablo Vazquez <venomgfx@gmail.com>
2015-05-19 01:35:37 +0200

    Swipebox support for previewing images and link url preview

--- blender-today/application/static/js/uglify/today-index-url-prettify.js
+++ blender-today/application/static/js/uglify/today-index-url-prettify.js
@@ -3,7 +3,11 @@ $(document).ready(function() {
 		var link = $(this).text();
 		var pretty = link.replace(/.*?:\/\//g, "").replace(/^www./, "").replace(/\/$/, "");
 		var prettier = pretty.substr(0, pretty.indexOf('/'))
-		$(this).text(prettier);
+		if (prettier != ''){
+			$(this).text(prettier);
+		} else {
+			$(this).text(pretty);
+		};
 		$(this).css('visibility', 'visible');
 	});
 });
