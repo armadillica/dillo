917c8d131397fb670424b50184a7f7c8ff02166b
Francesco Siddi <francesco.siddi@gmail.com>
2015-09-07 00:22:11 +0200

    New setup command for manage.py
    The command allows creation of an admin user from the cli.

--- dillo/manage.py
+++ dillo/manage.py
@@ -1,25 +1,48 @@
-from werkzeug import secure_filename
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate
 from flask.ext.migrate import MigrateCommand
 from application import app
 from application import db
-#from application import twitter_api
-from application import imgur_client
-from application.modules.posts.model import Post
-from application.modules.posts.model import PostRating
-from application.modules.posts.model import PostProperties
 from application.modules.users.model import user_datastore
-from application.helpers import encode_id
-from application.helpers import slugify
 
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 @manager.command
-def create_all_tables():
-    db.create_all()
+def setup():
+    import getpass
+    from flask_security.utils import encrypt_password
+    print("Please input the user data for the admin (you can edit later)")
+    while True:
+        admin_username = raw_input('Admin username:')
+        if len(admin_username) < 1:
+            print ("Username is too short")
+        elif user_datastore.find_user(username=admin_username):
+            print ("Username already in use")
+        else:
+            break
+    while True:
+        admin_password = getpass.getpass('Admin password:')
+        if len(admin_password) < 5:
+            print ("Password is too short")
+        else:
+            break
+    while True:
+        admin_email = raw_input('Admin email:')
+        if user_datastore.find_user(email=admin_email):
+            print ("Email aready in use")
+        else:
+            break
+    admin_role = user_datastore.find_or_create_role('admin')
+    admin = user_datastore.create_user(
+        username=admin_username,
+        password=encrypt_password(admin_password),
+        email=admin_email)
+    db.session.commit()
+    user_datastore.add_role_to_user(admin, admin_role)
+    db.session.commit()
+    print("Admin user succesfully created!")
 
 @manager.command
 def runserver():
@@ -27,94 +50,6 @@ def runserver():
     os.environ['DEBUG'] = 'true'
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
     app.run()
-
-@manager.command
-def populate_for_testing():
-    for x in range(50):
-        post = Post(
-            user_id=1,
-            category_id=1,
-            post_type_id=1,
-            title='test '+str(x),
-            slug='the-slug',
-            content='<p>content</p>')
-        db.session.add(post)
-        db.session.commit()
-        post.uuid = encode_id(post.id)
-        db.session.commit()
-        post_rating = PostRating(
-            post_id=post.id,
-            positive=0,
-            negative=0
-            )
-        db.session.add(post_rating)
-        post.update_hot()
-        db.session.commit()
-        print x
-
-# @manager.command
-# def twitter_fetch():
-#     # Fetch greatest id
-#     recent_tweets = twitter_api.search(q='#b3d -filter:retweets', rpp=100, page=2)
-#     print len(recent_tweets)
-#     for tweet in recent_tweets:
-#         #print dir(tweet)
-#         if tweet.favorite_count > 10:
-#             if not PostProperties.query\
-#                 .filter_by(field_type='tweet_id', value=tweet.id_str)\
-#                 .first():
-
-#                 bot = user_datastore.get_user('bot@blender.today')
-#                 # get category by parsing the tweet text
-#                 title = (tweet.text[:60] + '..') if len(tweet.text) > 60 else tweet.text
-#                 post = Post(
-#                     user_id=bot.id,
-#                     category_id=1,
-#                     post_type_id=1, #hardcoded to be a link
-#                     title=title,
-#                     slug=slugify(title),
-#                     content="https://twitter.com/{0}/status/{1}".format(
-#                         tweet.user.screen_name, tweet.id),
-#                     status='published'
-#                 )
-#                 db.session.add(post)
-#                 db.session.commit()
-#                 post.uuid = encode_id(post.id)
-#                 twitter_id = PostProperties(
-#                     post_id=post.id,
-#                     field_type='tweet_id',
-#                     value=tweet.id_str)
-#                 db.session.add(twitter_id)
-#                 db.session.commit()
-
-#                 post_rating = PostRating(
-#                     post_id=post.id,
-#                     positive=0,
-#                     negative=0
-#                     )
-#                 db.session.add(post_rating)
-#                 post.update_hot()
-#                 # if form.picture.data:
-#                 #     filename = secure_filename(form.picture.data.filename)
-#                 #     filepath = '/tmp/' + filename
-#                 #     form.picture.data.save(filepath)
-#                 #     image = imgur_client.upload_from_path(filepath, config=None, anon=True)
-#                 #     post.picture = image['id']
-#                 #     post.picture_deletehash = image['deletehash']
-#                 #     os.remove(filepath)
-#                 db.session.commit()
-#                 break
-
-#         print tweet.retweet_count
-#         print tweet.favorite_count
-#         print tweet.truncated
-#         print tweet.user.screen_name
-#         print "https://twitter.com/{0}/status/{1}".format(tweet.user.screen_name, tweet.id)
-#         print '----'
-#         #break
-#     # public_tweets = twitter_api.home_timeline()
-#     # for tweet in public_tweets:
-#     #     print tweet.url
 
 
 manager.run()
