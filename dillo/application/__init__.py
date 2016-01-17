7b46f1f32b09e7ca05f7768737b286c4b98ff022
Francesco Siddi <francesco.siddi@gmail.com>
2016-01-17 20:40:13 +0100

    Tweaks to installation process
    - Support for ProgrammingError exception (when Settings table is
    missing)
    - Support for disabling BugSnag (causes slowdown when working offline)

--- dillo/application/__init__.py
+++ dillo/application/__init__.py
@@ -7,6 +7,7 @@ from flask import session
 from flask import Blueprint
 
 from sqlalchemy.exc import OperationalError
+from sqlalchemy.exc import ProgrammingError
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.cache import Cache
 from flask_mail import Mail
@@ -52,12 +53,12 @@ if app.config.get('CACHE_REDIS_HOST') and app.config['CACHE_TYPE'] == 'redis':
 else:
     redis_client = None
 
-bugsnag.configure(
-  api_key = app.config['BUGSNAG_KEY'],
-  project_root = app.config['BUGSNAG_APP_PATH']
-)
-handle_exceptions(app)
-
+if app.config.get('BUGSNAG_KEY'):
+    bugsnag.configure(
+      api_key = app.config['BUGSNAG_KEY'],
+      project_root = app.config['BUGSNAG_APP_PATH']
+    )
+    handle_exceptions(app)
 
 # Config at https://console.developers.google.com/
 if app.config.get('SOCIAL_GOOGLE'):
@@ -147,6 +148,8 @@ from application.helpers.settings import load_settings
 try:
     load_settings()
 except OperationalError:
+    pass
+except ProgrammingError:
     pass
 
 @app.context_processor
