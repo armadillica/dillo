5910b503e05ad75372368a6b8dba2d0da3dbbd42
Francesco Siddi <francesco.siddi@gmail.com>
2017-03-15 23:13:01 +0100

    Introducing post creation

--- dillo/cli.py
+++ dillo/cli.py
@@ -5,10 +5,27 @@ import logging
 from flask import current_app
 from flask_script import Manager
 
+from pillar.cli import manager, create_service_account
+from pillar.api.utils import authentication
 from pillar.cli import manager
+
+import dillo.setup
 
 log = logging.getLogger(__name__)
 
 manager_dillo = Manager(current_app, usage="Perform Dillo operations")
 
 manager.add_command("dillo", manager_dillo)
+
+
+@manager_dillo.command
+@manager_dillo.option('-r', '--replace', dest='replace', action='store_true', default=False)
+def setup_for_dillo(project_url, replace=False):
+    """Adds Dillo node types to the project.
+
+    Use --replace to replace pre-existing Dillo node types
+    (by default already existing Dillo node types are skipped).
+    """
+
+    authentication.force_cli_user()
+    dillo.setup.setup_for_dillo(project_url, replace=replace)
