f1edc9a2303a6db3394243e5ed8c4862dbd88769
Francesco Siddi <francesco.siddi@gmail.com>
2017-04-10 22:08:45 +0200

    Add calculate hot

--- dillo/posts/eve_hooks.py
+++ dillo/posts/eve_hooks.py
@@ -13,24 +13,45 @@ On post creation:
 
 import itertools
 import logging
+import datetime
+from flask import current_app
 
-from dillo.node_types.post import node_type_post
 from pillar.api.nodes import only_for_node_type_decorator
+from dillo.node_types.post import node_type_post
+from dillo.utils.sorting import hot
 
 log = logging.getLogger(__name__)
 only_for_post = only_for_node_type_decorator(node_type_post['name'])
 
 
-@only_for_post
-def before_creating_post(post):
-    post['properties']['rating_positive'] = 0
-    post['properties']['rating_negative'] = 0
-    post['properties']['hot'] = 0
+def update_hot(item):
+    dt = item['_created']
+    dt = dt.replace(tzinfo=datetime.timezone.utc)
 
-def before_creating_posts(nodes):
-    for node in nodes:
-        before_creating_post(node)
+    item['properties']['hot'] = hot(
+        item['properties']['rating_positive'],
+        item['properties']['rating_negative'],
+        dt,
+    )
+
+
+@only_for_post
+def before_creating_post(item):
+    item['properties']['rating_positive'] = 0
+    item['properties']['rating_negative'] = 0
+    update_hot(item)
+
+
+def before_creating_posts(items):
+    for item in items:
+        before_creating_post(item)
+
+
+@only_for_post
+def after_replacing_post(item, original):
+    update_hot(item)
 
 
 def setup_app(app):
     app.on_insert_nodes += before_creating_posts
+    app.on_replace_nodes += after_replacing_post
