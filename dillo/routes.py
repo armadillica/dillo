7be261711efc10a2b89ac4ad695888e979188ea7
Francesco Siddi <francesco.siddi@gmail.com>
2017-05-24 20:27:57 +0200

    Aggregation pipeline for user ratings of posts

--- dillo/routes.py
+++ dillo/routes.py
@@ -1,10 +1,11 @@
 import logging
 
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, redirect, url_for, jsonify
+from flask_login import login_required, current_user
+from bson import ObjectId
 
-from pillar.web import system_util
 
-from dillo import current_dillo
+from pillar import current_app
 
 blueprint = Blueprint('dillo', __name__)
 log = logging.getLogger(__name__)
@@ -14,3 +15,43 @@ log = logging.getLogger(__name__)
 def index():
     return redirect(url_for('posts.index'))
 
+
+@login_required
+@blueprint.route('/ratings')
+def get_user_ratings():
+    """Aggregation pipeline for all posts rated by current user. Should be cached?
+    """
+
+    def get_ratings(user_id: ObjectId) -> list:
+        pipeline = [
+            {
+                '$match': {
+                    '_deleted': {'$ne': True},
+                    'properties.ratings': {'$exists': True}
+                }
+            },
+            {
+                '$project': {
+                    'properties.ratings': 1
+                }
+            },
+            {
+                '$unwind': {
+                    'path': "$properties.ratings",
+                }
+            },
+            {
+                '$match': {
+                    'properties.ratings.user': user_id
+                }
+            },
+        ]
+        c = current_app.db()['nodes']
+        # Return either a list with one item or an empty list
+        r = list(c.aggregate(pipeline=pipeline))
+        return r
+
+    ratings = get_ratings(ObjectId(current_user.objectid))
+    trim_ratings = [{str(r['_id']): r['properties']['ratings']['is_positive']} for r in ratings]
+
+    return jsonify(items=trim_ratings)
