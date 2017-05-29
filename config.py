ad25190eaeda043f65c5ee1f21a4a1c15aac7bea
Francesco Siddi <francesco.siddi@gmail.com>
2017-05-29 19:16:30 +0200

    Introducing default POST_CATEGORIES

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 # Everyone is allowed to rate. We determine validity of vote elsewhere.
 ROLES_FOR_COMMENT_VOTING = set()
+
+# This list generate the selection menu when creating or editing a post.
+POST_CATEGORIES = ['Artwork', 'Community', 'Development', 'Add-Ons', 'Education', 'Tutorials',
+                   'Resources']
