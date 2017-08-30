55ceed9ddcffd510077e34ab0eb5ae9012a67b02
Pablo Vazquez <venomgfx@gmail.com>
2017-08-31 01:06:23 +0200

    User Space: Style listing

--- dillo/users/routes.py
+++ dillo/users/routes.py
@@ -5,6 +5,7 @@ from flask import abort, current_app, render_template
 from pillarsdk import Node
 from pillar.web.users.routes import blueprint
 from pillar.web import system_util
+from pillar.web.utils import get_file
 
 log = logging.getLogger(__name__)
 
@@ -12,7 +13,7 @@ log = logging.getLogger(__name__)
 @blueprint.route('/u/<username>')
 def users_view(username):
     """View public user page.
-    
+
     Use direct db call to retrieve the user and then use the api to query the paginated list of all
     published dillo_posts from the user.
 
@@ -27,5 +28,9 @@ def users_view(username):
         'where': {'user': str(user['_id']), 'node_type': 'dillo_post', 'properties.status': 'published'},
         'sort': '-_created',
     }, api=api)
+
+    for post in posts['_items']:
+        if post.picture:
+            post.picture = get_file(post.picture, api=api)
 
     return render_template('dillo/user.html', user=user, posts=posts)
