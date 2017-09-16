edd4390655ec2f0f0a158c0a7d6f3b8790977db8
Francesco Siddi <francesco.siddi@gmail.com>
2017-09-16 20:53:58 +0200

    Test for post creation when user is not part of dillo_user_main

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,4 @@
-import json
+import copy
 import pillar.tests.common_test_data as ctd
 from abstract_dillo_test import AbstractDilloTest
 
@@ -12,8 +12,7 @@ class NodeOwnerTest(AbstractDilloTest):
         self.create_valid_auth_token(self.user_id, 'token')
         self.project_id, _ = self.ensure_project_exists()
 
-    def test_create_with_explicit_owner(self):
-        test_node = {
+        self.test_node = {
             'project': self.project_id,
             'node_type': 'dillo_post',
             'name': 'test with user',
@@ -22,18 +21,22 @@ class NodeOwnerTest(AbstractDilloTest):
                 'category': 'Fün'
             },
         }
+
+    def test_create_with_explicit_owner(self):
+        test_node = copy.deepcopy(self.test_node)
         self._test_user(test_node)
 
     def test_create_with_implicit_owner(self):
-        test_node = {
-            'project': self.project_id,
-            'node_type': 'dillo_post',
-            'name': 'test with user',
-            'properties': {
-                'category': 'Fün'
-            },
-        }
+        test_node = copy.deepcopy(self.test_node)
+        del(test_node['user'])
         self._test_user(test_node)
+
+    def test_create_with_non_main_user(self):
+        non_main_user_id = self.create_user(user_id='cafef005972666988bef660f')
+        test_node = copy.deepcopy(self.test_node)
+        self.create_valid_auth_token(non_main_user_id, 'non_main_token')
+        test_node['user'] = non_main_user_id
+        self.post('/api/nodes', json=test_node, auth_token='non_main_token', expected_status=403)
 
     def _test_user(self, test_node):
         resp = self.post('/api/nodes', json=test_node, auth_token='token',
