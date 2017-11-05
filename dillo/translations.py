9e1247f47f1a9fd4ec2bcf1333a189d9c323a89c
Dalai Felinto <dfelinto@gmail.com>
2017-11-05 16:05:51 -0200

    A new file for semi-static placeholder of strings to be translated
    This is tricky because these values can change.
    However for things like communities summary (and perhaps description?)
    we can have them translated this way.

---
+++ dillo/translations.py
@@ -0,0 +1,9 @@
+# Placeholders for translation strings
+from flask_babel gettext as _
+
+# Translation of community summaries.
+communities = {
+    _('Community-driven Blender news'),
+    _('Comunidad de Habla Hispana'),
+    _('Ideas for Blender'),
+}
