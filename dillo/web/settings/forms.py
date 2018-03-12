8afcdf6abf3cfcacee20ce57492469e475a57d23
Francesco Siddi <francesco.siddi@gmail.com>
2018-03-12 20:39:46 +0100

    Do not require CSRF for embeded for items of a FieldList

--- dillo/web/settings/forms.py
+++ dillo/web/settings/forms.py
@@ -1,13 +1,13 @@
+import wtforms
+from wtforms.validators import URL, DataRequired
 from flask_wtf import Form
 from flask_wtf.html5 import URLField
-from wtforms import StringField, FieldList, FormField
-from wtforms.validators import URL, DataRequired
 
 
-class LinkForm(Form):
-    name = StringField('Name', validators=[DataRequired()])
+class LinkForm(wtforms.Form):
+    name = wtforms.StringField('Name', validators=[DataRequired()])
     url = URLField(validators=[DataRequired(), URL()])
 
 
 class LinksListForm(Form):
-    links = FieldList(FormField(LinkForm), max_entries=5)
+    links = wtforms.FieldList(wtforms.FormField(LinkForm), max_entries=5)
