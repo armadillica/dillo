76d360f1198e31fcbcad30c36a60d775f1d5f5a0
Francesco Siddi <francesco.siddi@gmail.com>
2019-03-18 14:48:41 +0100

    Update error message for missing readlink

--- deploy/2docker.sh
+++ deploy/2docker.sh
@@ -4,7 +4,7 @@ DEPLOY_BRANCH=${DEPLOY_BRANCH:-production}
 
 # macOS does not support readlink -f, so we use greadlink instead
 if [[ `uname` == 'Darwin' ]]; then
-    command -v greadlink 2>/dev/null 2>&1 || { echo >&2 "Install greadlink using brew."; exit 1; }
+    command -v greadlink 2>/dev/null 2>&1 || { echo >&2 "Install coreutils using brew."; exit 1; }
     readlink='greadlink'
 else
     readlink='readlink'
