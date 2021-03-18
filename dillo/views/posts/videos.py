87b516a14ac99064188dd913af3d1919aaaea0d8
Francesco Siddi <francesco.siddi@gmail.com>
2021-03-18 01:46:45 +0100

    Update view count to use Video model

--- dillo/views/posts/videos.py
+++ dillo/views/posts/videos.py
@@ -2,12 +2,12 @@ from django.db.models import F
 from django.http import JsonResponse
 from django.views import View
 
-from dillo.models.posts import PostMediaVideo
+from dillo.models.posts import Video
 
 
 class VideoViewsCountIncreaseView(View):
     def post(self, request, *args, **kwargs):
-        PostMediaVideo.objects.filter(pk=self.kwargs['video_id']).update(
+        Video.objects.filter(static_asset_id=self.kwargs['video_id']).update(
             views_count=F('views_count') + 1
         )
         return JsonResponse({'status': 'ok'})
