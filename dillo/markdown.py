b497676fe87ccdf8cce4332273acf9a832b1d4a4
Pablo Vazquez <pablo@blender.org>
2021-06-17 16:49:40 +0200

    Filters: mistune plugin for <kbd> tags

--- dillo/markdown.py
+++ dillo/markdown.py
@@ -34,6 +34,26 @@ def plugin_shortcode_with_link(md):
     md.inline.rules.append('shortcode_link')
     md.renderer.register('shortcode_link', keep_shortcode_link)
 
+# Regex to look for <kbd> tags, such as [[ctrl+X]]
+KBD_TAG_PATTERN = (
+    r'\[\['        # [[
+    r'([\s\S]+?)'  # Text
+    r'\]\](?!\])'  # ]]
+)
+
+def parse_html_kbd(inline, m, state):
+    text = m.group(1)
+    return 'kbd', text
+
+def render_html_kbd(keyt):
+    return f'<kbd>{keyt}</kbd>'
+
+def plugin_html_kbd(md):
+    """Define a plugin that converts [[key]] to <kbd>key</kbd>."""
+    md.inline.register_rule('kbd', KBD_TAG_PATTERN, parse_html_kbd)
+    md.inline.rules.append('kbd')
+    md.renderer.register('kbd', render_html_kbd)
+
 
 def render(text: str) -> Markup:
     """Render given text as markdown."""
@@ -41,7 +61,10 @@ def render(text: str) -> Markup:
 
     if _markdown is None:
         _markdown = mistune.create_markdown(
-            escape=True, plugins=[plugin_shortcode_with_link, mistune.plugins.extra.plugin_url],
+            escape=True, plugins=[
+                plugin_shortcode_with_link,
+                mistune.plugins.extra.plugin_url,
+                plugin_html_kbd],
         )
 
     return Markup(_markdown(text))
