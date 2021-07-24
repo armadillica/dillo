beec5fdab0695a863bfe8599c76df04d2649057e
Pablo Vazquez <pablo@blender.org>
2021-07-25 01:44:12 +0200

    Groups: do not consider superuser as Moderator

--- dillo/context_processors.py
+++ dillo/context_processors.py
@@ -68,8 +68,7 @@ def current_user_js(request):
     current_user = {
         'isAuthenticated': request.user.is_authenticated,
         'isStaff': request.user.is_staff,
-        'isModerator': request.user.groups.filter(name='moderators').exists()
-        or request.user.is_superuser,
+        'isModerator': request.user.groups.filter(name='moderators').exists(),
         'username': (None if not request.user.is_authenticated else request.user.username),
         'avatar': None,
         'url': (None if not request.user.is_authenticated else request.user.profile.absolute_url),
